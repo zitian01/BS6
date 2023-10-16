3f8a0fdb769c9463ae81317cdfb8a9263996bf4b
zitian <zitianchen01@gmail.com>
2023-10-15 22:41:56 -0400

    Add project files.

---
+++ BS6/Controllers/StreamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BS6.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class StreamController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateStream([FromBody] string streamName)
+        {
+            string filePath = $"wwwroot/{streamName}.html";
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return BadRequest("Stream name already exists");
+            }
+
+            // Generate your HTML content here. Simplified for this example.
+            string htmlContent = $"<html><body>Stream: {streamName}</body></html>";
+
+            System.IO.File.WriteAllText(filePath, htmlContent);
+
+            return Ok($"New stream created at {filePath}");
+        }
+    }
+}
